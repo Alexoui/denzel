60a19ada4444f507234d2f9a0fddac16b10e180a
Alexoui <alexandre.caro@devinci.fr>
2019-03-18 18:55:26 +0100

    new update2

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.listen(9292, () => {
 
 app.get("/movies/populate", async (request, response) => {
   const movies = await imdb(DENZEL_IMDB_ID);
-  console.log( `${movies.length} movies found.`);
+  console.log(`${movies.length} movies found.`);
   collection.insertMany(movies, (error, result) => {
     if (error) {
       return response.status(500).send(error);
@@ -42,20 +42,70 @@ app.get("/movies/populate", async (request, response) => {
   });
 });
 
+app.get("/movies/search", (request, response) => {
+  var metascore = request.query.metascore;
+  var limit = request.query.limit;
+  if (metascore == undefined) {
+    metascore = 0;
+  }
+  if (limit == undefined) {
+    limit = 5;
+  }
+  collection.aggregate([{
+    $match: {
+      metascore: {
+        $gte: Number(metascore)
+      }
+    }
+  }, {
+    $sample: {
+      size: Number(limit)
+    }
+  }, {
+    $sort: {
+      "metascore": -1
+    }
+  }]).toArray((error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
+});
+
 app.get("/movies", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+  collection.find({}).toArray((error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
 });
 
 app.get("/movies/:id", (request, response) => {
-    collection.findOne({ "id": request.params.id }, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+  collection.findOne({
+    "id": request.params.id
+  }, (error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
+});
+
+
+app.post("/movies/:id", (request, response) => {
+  collection.updateOne({
+    "id": request.params.id
+  }, {
+    $set: {
+      "date": request.query.date,
+      "review": request.query.review
+    }
+  }, (error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result.result);
+  });
 });
